c0a92a51b3773c112ba618f99805f271dc4b2d73
TheRenkroomsControllerRRSJ <hello.brawl.stars.and.roblox@gmail.com>
2025-10-24 21:35:10 +0300

    al
    al

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+from get_class import get_class
+import os, random
+import requests
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''The duck command returns the photo of the duck'''
+    print('hello')
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("You forgot to upload the image :(")
+
+bot.run("MTM2NzkyNDU1NTg3NTg3NjkyNQ.GPkrw_.sqiuiFmG0ZaJWor-z5kzbxph0IMuTvtZsSdwNs")
